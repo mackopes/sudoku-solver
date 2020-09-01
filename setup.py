b7d7ac661192f3c051cabd5bf77becd711ea0b18
mackopes <pialamar@gmail.com>
2020-09-01 23:11:50 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="sudoku-solver-mackopes",
+    version="0.1",
+    author="mackopes",
+    author_email="pialamar@gmail.com",
+    description="A simple sudoku solver",
+    url="https://github.com/mackopes/sudoku-solver",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
